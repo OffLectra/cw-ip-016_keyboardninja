8034577ff44185178e7f96fca47936ce0d1a2842
Ilya <ilyazaytsev2002@bk.ru>
2021-05-06 19:35:39 +0700

    Add file reading

--- src/libKBNinja/load_lvl.cpp
+++ src/libKBNinja/load_lvl.cpp
@@ -1,6 +1,17 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+
 using namespace std;
+
 void load_lvl(string language, int lvl)
 {
+    string path = "src/resources/control.txt";
+    ifstream file(path);
+    if (!file.is_open())
+        cout << "Error open file!";
+    else {
+    
+    }
 }
 
