7eae8b83a1e6d8051fa2fc40a840741696eb2f7f
Ilya <ilyazaytsev2002@bk.ru>
2021-05-10 18:15:35 +0700

    Added the function of finding the desired line in the file

--- src/libKBNinja/load_lvl.cpp
+++ src/libKBNinja/load_lvl.cpp
@@ -7,11 +7,19 @@ using namespace std;
 void load_lvl(string language, int lvl)
 {
     string path = "src/resources/control.txt";
+    string str;
     ifstream file(path);
     if (!file.is_open())
         cout << "Error open file!";
     else {
-    
+        int number_current_str = 0;
+        while (getline(file, str)) {
+            cout << number_current_str << endl;
+            if (number_current_str == lvl) {
+                break;
+            }
+            number_current_str++;
+        }
+        cout << str << endl;
     }
 }
-
