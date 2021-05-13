0d229eb896932c60aba1fee73312c23f159ce1eb
Alexey <alexegoshin0403@yandex.ru>
2021-05-13 10:52:06 +0300

    Added a function for changing terminal parameters

--- src/libKBNinja/getch.cpp
+++ src/libKBNinja/getch.cpp
@@ -1,4 +1,18 @@
 #include <cstdio>
+#include <termios.h>
+
+static struct termios parameters;
+
+/*
+new_terminal - procedure for changing terminal parameters
+*/
+void new_terminal()
+{
+    tcgetattr(0, &parameters);
+    parameters.c_lflag &= ~ICANON;
+    parameters.c_lflag &= ~ECHO;
+    tcsetattr(0, TCSANOW, &parameters);
+}
 
 /*
 getch - 1 character reading function
@@ -6,6 +20,7 @@ getch - 1 character reading function
 char getch()
 {
     char characte;
+    new_terminal();
     characte = getchar();
     return characte;
-}
+}	
