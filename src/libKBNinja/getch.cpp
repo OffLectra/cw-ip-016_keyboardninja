3c61593038347d3a599fcc35c971e3c20674a96d
Alexey <alexegoshin0403@yandex.ru>
2021-05-13 11:09:01 +0300

    Fixed an error with the terminal continuing to work after changing the parameters

--- src/libKBNinja/getch.cpp
+++ src/libKBNinja/getch.cpp
@@ -1,17 +1,26 @@
 #include <cstdio>
 #include <termios.h>
 
-static struct termios parameters;
+static struct termios old_parameters, new_parameters;
 
 /*
 new_terminal - procedure for changing terminal parameters
 */
 void new_terminal()
 {
-    tcgetattr(0, &parameters);
-    parameters.c_lflag &= ~ICANON;
-    parameters.c_lflag &= ~ECHO;
-    tcsetattr(0, TCSANOW, &parameters);
+    tcgetattr(0, &old_parameters);
+    new_parameters = old_parameters;
+    new_parameters.c_lflag &= ~ICANON;
+    new_parameters.c_lflag &= ~ECHO;
+    tcsetattr(0, TCSANOW, &new_parameters);
+}
+
+/*
+old_terminal - procedure for restoring old terminal parameters
+*/
+void old_terminal()
+{
+    tcsetattr(0, TCSANOW, &old_parameters);
 }
 
 /*
@@ -22,5 +31,6 @@ char getch()
     char characte;
     new_terminal();
     characte = getchar();
+    old_terminal();
     return characte;
-}	
+}
