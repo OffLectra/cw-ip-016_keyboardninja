562cdfa34d81edc2bc5c62134e68e0400b382651
Alexey <alexegoshin0403@yandex.ru>
2021-05-15 10:25:08 +0300

    Create function for counting the number of lines in a file

---
+++ src/libKBNinja/count_number_of_str.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int count_number_of_str(string path)
+{
+    ifstream file(path);
+
+    int count_str = 0;
+    string str;
+    while (!file.eof()) {
+        getline(file, str);
+        count_str++;
+    }
+
+    return count_str;
+}
