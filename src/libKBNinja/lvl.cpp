48352f820d5f3d328e4a163a5f46acd90d7e20c5
Alexey <alexegoshin0403@yandex.ru>
2021-05-16 09:22:29 +0300

    Added the output of elements in accordance with the purpose

--- src/libKBNinja/lvl.cpp
+++ src/libKBNinja/lvl.cpp
@@ -1,13 +1,15 @@
-#include "lvl.h"
 #include "count_number_of_str.h"
+#include "lvl.h"
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-void lvl(string path)
+void lvl(string path, int purpose)
 {
+    srand(time(NULL));
     ifstream file(path);
 
     if (!file.is_open()) {
@@ -15,6 +17,18 @@ void lvl(string path)
     } else {
         int number_lines = count_number_of_str(path);
 
-        cout << number_lines << endl;
+        for (int i = 0; i < purpose; ++i) {
+            string str;
+            int choise_sentence = rand() % number_lines;
+            int number_current_str = 0;
+            file.seekg(0);
+            while (getline(file, str)) {
+                if (number_current_str == choise_sentence) {
+                    break;
+                }
+                number_current_str++;
+            }
+            cout << str << endl;
+        }
     }
 }
