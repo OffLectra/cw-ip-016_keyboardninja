2d688bcc7d3754dbf7c3aeb83a102d139fe9d391
Maxim <maxuswill@yandex.ru>
2021-05-20 04:25:09 -0400

    Add choose item menu

--- src/libKBNinja/menu.cpp
+++ src/libKBNinja/menu.cpp
@@ -1,11 +1,14 @@
+#include "getch.h"
 #include "menu.h"
 #include <iostream>
-
+using namespace std;
 int menu()
 {
     int key = 0;
-
+    int input;
     do {
+        system("clear");
+        key = (key + 4) % 4;
         if (key == 0) {
             cout << "<New game>" << endl << endl << endl;
         } else {
@@ -29,8 +32,16 @@ int menu()
         } else {
             cout << "Exit" << endl << endl << endl;
         }
+        input = getch();
+        if (input == 224) {
+            input = getch();
+            if (input == 119)
+                key++;
+            if (input == 115)
+                key--;
+        }
+    } while (input != 10);
 
-    } while (1);
-
+    system("clear");
     return key;
 }
