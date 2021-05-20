cafbddfe0d5abbbeea8c193d4c9c598a5f0b9832
Maxim <maxuswill@yandex.ru>
2021-05-20 03:34:24 -0400

    Add menu items

--- src/libKBNinja/menu.cpp
+++ src/libKBNinja/menu.cpp
@@ -1,5 +1,36 @@
+#include "menu.h"
 #include <iostream>
 
 int menu()
 {
+    int key = 0;
+
+    do {
+        if (key == 0) {
+            cout << "<New game>" << endl << endl << endl;
+        } else {
+            cout << "New game" << endl << endl << endl;
+        }
+
+        if (key == 1) {
+            cout << "<Continue>" << endl << endl << endl;
+        } else {
+            cout << "Continue" << endl << endl << endl;
+        }
+
+        if (key == 2) {
+            cout << "<About>" << endl << endl << endl;
+        } else {
+            cout << "About" << endl << endl << endl;
+        }
+
+        if (key == 3) {
+            cout << "<Exit>" << endl << endl << endl;
+        } else {
+            cout << "Exit" << endl << endl << endl;
+        }
+
+    } while (1);
+
+    return key;
 }
