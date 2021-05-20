cde1ceb6a6742e81fc7634bf9819e556e9fb724a
Alexey <alexegoshin0403@yandex.ru>
2021-05-20 16:46:35 +0300

    Moved the line selection function to a separate file

--- src/libKBNinja/lvl.cpp
+++ src/libKBNinja/lvl.cpp
@@ -1,6 +1,6 @@
 #include "count_number_of_str.h"
 #include "lvl.h"
-#include <ctime>
+#include "select_str.h"
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -9,26 +9,11 @@ using namespace std;
 
 void lvl(string path, int purpose)
 {
-    srand(time(NULL));
-    ifstream file(path);
+    int number_lines = count_number_of_str(path);
 
-    if (!file.is_open()) {
-        cout << "Error open file" << endl;
-    } else {
-        int number_lines = count_number_of_str(path);
+    for (int i = 0; i < purpose; ++i) {
+        string str = select_str(path, number_lines);
 
-        for (int i = 0; i < purpose; ++i) {
-            string str;
-            int choise_sentence = rand() % number_lines;
-            int number_current_str = 0;
-            file.seekg(0);
-            while (getline(file, str)) {
-                if (number_current_str == choise_sentence) {
-                    break;
-                }
-                number_current_str++;
-            }
-            cout << str << endl;
-        }
+        cout << str << endl;
     }
 }
