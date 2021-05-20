659c8c4b4eeb532a410a5690f20eafced0b1f653
Alexey <alexegoshin0403@yandex.ru>
2021-05-20 20:02:36 +0300

    Added character input

--- src/libKBNinja/lvl.cpp
+++ src/libKBNinja/lvl.cpp
@@ -1,7 +1,7 @@
 #include "lvl.h"
 #include "count_number_of_str.h"
+#include "getch.h"
 #include "select_str.h"
-#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,8 +16,17 @@ void lvl(string path, int purpose)
     used_str.reserve(purpose);
 
     for (int i = 0; i < purpose; ++i) {
+        system("clear");
         string str = select_str(path, number_lines, used_str);
 
         cout << str << endl;
+
+        for (unsigned int i = 0; i < str.length(); ++i) {
+            char tmp = getch();
+            while (tmp != str[i]) {
+                tmp = getch();
+            }
+            cout << tmp;
+        }
     }
 }
