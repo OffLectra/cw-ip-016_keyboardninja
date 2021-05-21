28d5736d47b78881545c1e68d22fe012262fbc31
Alexey <alexegoshin0403@yandex.ru>
2021-05-21 18:08:39 +0300

    Add counting errors and time

--- src/libKBNinja/lvl.cpp
+++ src/libKBNinja/lvl.cpp
@@ -1,7 +1,9 @@
 #include "lvl.h"
 #include "count_number_of_str.h"
 #include "getch.h"
+#include "results.h"
 #include "select_str.h"
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,6 +17,11 @@ void lvl(string path, int purpose)
     vector<int> used_str;
     used_str.reserve(purpose);
 
+    int errors = 0;
+    clock_t time_start, time_end;
+
+    time(&time_start);
+
     for (int i = 0; i < purpose; ++i) {
         system("clear");
         string str = select_str(path, number_lines, used_str);
@@ -24,9 +31,16 @@ void lvl(string path, int purpose)
         for (unsigned int i = 0; i < str.length(); ++i) {
             char tmp = getch();
             while (tmp != str[i]) {
+                errors++;
                 tmp = getch();
             }
             cout << tmp;
         }
     }
+
+    time(&time_end);
+
+    double time = difftime(time_end, time_start);
+
+    results(errors, time);
 }
