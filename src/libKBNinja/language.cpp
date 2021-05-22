692be1252f34b5d0fbb620c3922add3c71f2b0e6
Maxim <maxuswill@yandex.ru>
2021-05-22 06:59:47 -0400

    Add parameters for choose language

--- src/libKBNinja/language.cpp
+++ src/libKBNinja/language.cpp
@@ -1,7 +1,25 @@
 #include "getch.h"
+#include "language.h"
 #include <iostream>
 
 using namespace std;
 int language()
 {
+    int key = 0;
+    int input;
+    do {
+        cout << "Choose language:" << endl;
+        key = (key + 2) % 2;
+        if (key == 0)
+            cout << "<Russian>" << endl;
+        else
+            cout << "Russian" << endl;
+
+        if (key == 1)
+            cout << "<English>" << endl;
+        else
+            cout << "English" << endl;
+
+    } while ();
+    return key;
 }
