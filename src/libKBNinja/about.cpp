3d2892937a285179523d0ec7255ce643984a8e97
Max <maxuswill@yandex.ru>
2021-05-25 02:49:41 -0400

    Add autors of game in description

--- src/libKBNinja/about.cpp
+++ src/libKBNinja/about.cpp
@@ -1,6 +1,16 @@
+#include "about.h"
+#include "getch.h"
 #include <iostream>
+using namespace std;
 
 void about()
 {
-}
+    int input;
+    cout << "Programm made in 2021" << endl;
+    cout << "       Autors:       " << endl;
+    cout << "    Alexey Egoshin   " << endl;
+    cout << "     Maxim Valter    " << endl;
+    cout << "     Ilya Zaicev     " << endl << endl;
 
+    cout << "For exit press Enter" << endl;
+}
