26727f3667aa2706f0052e214e6ad1319a6ae48b
Ilya <ilyazaytsev2002@bk.ru>
2021-05-27 14:33:02 +0700

    Added file path selection

--- src/libKBNinja/load_lvl.cpp
+++ src/libKBNinja/load_lvl.cpp
@@ -1,3 +1,4 @@
+#include "choose_path.h"
 #include "get_str.h"
 #include "load_lvl.h"
 #include "seporate_str.h"
@@ -11,13 +12,11 @@ using namespace std;
 void load_lvl(string language, int lvl)
 {
     string control_lvl = get_str(lvl);
-    string lvl_file;
 
     int purpose;
     double time;
     int errors;
     seporate_str(control_lvl, purpose, time, errors);
 
-    
+    string path = choose_path(language, lvl);
 }
-
