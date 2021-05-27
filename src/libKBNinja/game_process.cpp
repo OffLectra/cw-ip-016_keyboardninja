9d1e0a37ff25b7cafa6eb391a120227143daa0fe
Max <maxuswill@yandex.ru>
2021-05-27 08:20:25 -0400

    Add main menu

--- src/libKBNinja/game_process.cpp
+++ src/libKBNinja/game_process.cpp
@@ -1,4 +1,5 @@
 #include "language.h"
+#include "menu.h"
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,5 +7,21 @@ using namespace std;
 void game_process()
 {
     string lang = language();
-    cout << lang;
+    bool is_menu = true;
+    int menu_item;
+
+    do {
+        menu_item = menu();
+        switch (menu_item) {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            is_menu = false;
+            break;
+        }
+    } while (is_menu == true);
 }
