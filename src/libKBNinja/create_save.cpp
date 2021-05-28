2d1a2cac1e9383c762d77240651e21f1cc705399
Ilya <ilyazaytsev2002@bk.ru>
2021-05-28 16:05:07 +0700

    Added level saves

--- src/libKBNinja/create_save.cpp
+++ src/libKBNinja/create_save.cpp
@@ -5,13 +5,14 @@
 
 using namespace std;
 
-void create_save(string language)
+void create_save(string language, int lvl)
 {
     string path = "src/resources/save.txt";
-
     ofstream file(path);
     if (file.is_open()) {
         file << language << endl;
+        file << lvl << endl;
     }
+
     file.close();
 }
