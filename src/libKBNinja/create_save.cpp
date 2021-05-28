e45fc6135837632530e3a6c95d9f692e9331bfd2
Ilya <ilyazaytsev2002@bk.ru>
2021-05-28 16:10:17 +0700

    Added whether the next level is open

--- src/libKBNinja/create_save.cpp
+++ src/libKBNinja/create_save.cpp
@@ -5,14 +5,17 @@
 
 using namespace std;
 
-void create_save(string language, int lvl)
+void create_save(string language, int lvl, bool is_next_lvl)
 {
     string path = "src/resources/save.txt";
     ofstream file(path);
     if (file.is_open()) {
         file << language << endl;
         file << lvl << endl;
+        if (is_next_lvl)
+            file << 1;
+        else
+            file << 0;
     }
-
     file.close();
 }
