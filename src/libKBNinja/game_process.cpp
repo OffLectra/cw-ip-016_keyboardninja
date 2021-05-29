693c50178c0170cf522957223297b366bb53754c
Max <maxuswill@yandex.ru>
2021-05-29 05:19:39 -0400

    Add fuction choose levels and new game

--- src/libKBNinja/game_process.cpp
+++ src/libKBNinja/game_process.cpp
@@ -1,22 +1,52 @@
 #include "about.h"
+#include "error_lvl.h"
 #include "language.h"
+#include "levels.h"
+#include "load_save.h"
 #include "menu.h"
+#include "new_game_save.h"
+#include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
 
 void game_process()
 {
-    string lang = language();
+    string lang;
     bool is_menu = true;
+    bool level = false;
+    int lvl;
+    int chooselvl = 0;
     int menu_item;
-
+    load_save(lang, lvl);
     do {
         menu_item = menu();
         switch (menu_item) {
         case 0:
+            lang = language();
+            lvl = 1;
+            new_game(lang, lvl);
+            is_menu = false;
             break;
         case 1:
+            level = false;
+            is_menu = false;
+            if (lang != "") {
+                do {
+                    chooselvl = levels();
+                    if (chooselvl != 20) {
+                        if (chooselvl > lvl) {
+                            error_lvl();
+                            chooselvl = 0;
+                        } else
+                            level = true;
+                    } else {
+                        level = true;
+                        is_menu = true;
+                    }
+                } while (level == false);
+            }
+            else is_menu = true;
             break;
         case 2:
             about();
@@ -26,4 +56,5 @@ void game_process()
             break;
         }
     } while (is_menu == true);
+    cout << lang << lvl;
 }
