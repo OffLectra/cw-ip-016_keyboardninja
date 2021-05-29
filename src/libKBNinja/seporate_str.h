75f79537b5798af34cf8d46aa0cf13f026146d35
Max <maxuswill@yandex.ru>
2021-05-29 14:14:29 -0400

    Fixed bags

--- src/libKBNinja/seporate_str.h
+++ src/libKBNinja/seporate_str.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <iostream>
 #include <string>
-void seporate_str(std::string str, int purpose, double time, int errors);
+void seporate_str(std::string str, int& purpose, double& time, int& errors);
