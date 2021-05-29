eaa56d0d752f47879f633f85f97c101f0bcdcf42
Max <maxuswill@yandex.ru>
2021-05-29 14:10:30 -0400

    Add nextlvl

--- src/libKBNinja/lvl.h
+++ src/libKBNinja/lvl.h
@@ -8,4 +8,4 @@
 
 #include <iostream>
 #include <string>
-void lvl(std::string path, int purpose, int max_errors, double max_time);
+void lvl(std::string path, int purpose, int max_errors, double max_time, bool& is_next_lvl);
