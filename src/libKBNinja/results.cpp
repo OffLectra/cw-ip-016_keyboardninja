1c3c5825d601d154e74095a6aea5c844aa93d05b
Max <maxuswill@yandex.ru>
2021-05-29 14:12:29 -0400

    Add parameter for nextlvl

--- src/libKBNinja/results.cpp
+++ src/libKBNinja/results.cpp
@@ -1,12 +1,17 @@
 #include "results.h"
+#include "getch.h"
 #include <iostream>
 
 using namespace std;
 
-void results(int errors, double time, int number_characters, int pass_lvl)
+void results(
+        int errors,
+        double time,
+        int number_characters,
+        int pass_lvl,
+        bool& is_next_lvl)
 {
     system("clear");
-
     double accuracy = ((number_characters - errors) * 100) / number_characters;
     if (accuracy < 0.0)
         accuracy = 0.0;
@@ -21,15 +26,25 @@ void results(int errors, double time, int number_characters, int pass_lvl)
     switch (pass_lvl) {
     case lvl_pass:
         cout << "Level passed!" << endl;
+        is_next_lvl = true;
         break;
     case over_time:
         cout << "Level not passed: time exceeded!" << endl;
+        is_next_lvl = false;
         break;
     case over_errors:
         cout << "Level not passed: number of errors exceeded!" << endl;
+        is_next_lvl = false;
         break;
     case over_time_errors:
         cout << "Level not passed: time and number of errors exceeded!" << endl;
+        is_next_lvl = false;
         break;
     }
+    int input;
+    cout << endl << "For exit press Enter" << endl;
+    do {
+        input = getch();
+    } while (input != 10);
+    system("clear");
 }
--- src/libKBNinja/results.h
+++ src/libKBNinja/results.h
@@ -1,5 +1,4 @@
 #pragma once
 
 #include "constants.h"
-
-void results(int errors, double time, int number_characters, int pass_lvl);
+void results(int errors, double time, int number_characters, int pass_lvl, bool& is_next_lvl);
