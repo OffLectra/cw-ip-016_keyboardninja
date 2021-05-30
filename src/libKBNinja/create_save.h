88506128b7e720adfcc5456cf9b9a5c8adaa56c9
Alexey <alexegoshin0403@yandex.ru>
2021-05-30 13:52:57 +0300

    Delete extern include

--- src/libKBNinja/create_save.h
+++ src/libKBNinja/create_save.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <string>
 
-using namespace std;
-
-void create_save(string language, int lvl, bool is_next_lvl);
+void create_save(sf:: string language, int lvl, bool is_next_lvl);
