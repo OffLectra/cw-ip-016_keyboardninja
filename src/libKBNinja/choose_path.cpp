dcbd617e9977f7e4fa5376e63c66b9d71533d19d
Alexey <alexegoshin0403@yandex.ru>
2021-05-30 13:30:23 +0300

    Add comments and break in if()

--- src/libKBNinja/choose_path.cpp
+++ src/libKBNinja/choose_path.cpp
@@ -1,42 +1,65 @@
 #include "choose_path.h"
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-const string file_path[]
-        = {"src/resources/symbol_RU.txt",
-           "src/resources/words_RU.txt",
-           "src/resources/sentence_RU.txt",
-           "src/resources/sparagraph_RU.txt",
-           "src/resources/symbol_EN.txt",
-           "src/resources/words_EN.txt",
-           "src/resources/sentence_EN.txt",
-           "src/resources/sparagraph_EN.txt"};
+/*
+choose_path - this function selects the path to the desired file with the
+elements to enter
+*/
 
 string choose_path(string language, int lvl)
 {
+    const string file_path[]
+            = {"src/resources/symbol_RU.txt",
+               "src/resources/words_RU.txt",
+               "src/resources/sentence_RU.txt",
+               "src/resources/sparagraph_RU.txt",
+               "src/resources/symbol_EN.txt",
+               "src/resources/words_EN.txt",
+               "src/resources/sentence_EN.txt",
+               "src/resources/sparagraph_EN.txt"};
     string path;
 
     if (language == "RU") {
-        if (lvl >= 1 && lvl <= 3)
+        if (lvl >= 1 && lvl <= 3) {
             path = file_path[0];
-        if (lvl >= 4 && lvl <= 6)
+            break;
+        }
+        if (lvl >= 4 && lvl <= 6) {
             path = file_path[1];
-        if (lvl >= 7 && lvl <= 9)
+            break;
+        }
+        if (lvl >= 7 && lvl <= 9) {
             path = file_path[2];
-        if (lvl >= 10 && lvl <= 12)
+            break;
+        }
+        if (lvl >= 10 && lvl <= 12) {
             path = file_path[3];
+            break;
+        }
     }
+
     if (language == "EN") {
-        if (lvl >= 1 && lvl <= 3)
+        if (lvl >= 1 && lvl <= 3) {
             path = file_path[4];
-        if (lvl >= 4 && lvl <= 6)
+            break;
+        }
+        if (lvl >= 4 && lvl <= 6) {
             path = file_path[5];
-        if (lvl >= 7 && lvl <= 9)
+            break;
+        }
+        if (lvl >= 7 && lvl <= 9) {
             path = file_path[6];
-        if (lvl >= 10 && lvl <= 12)
+            break;
+        }
+        if (lvl >= 10 && lvl <= 12) {
             path = file_path[7];
+            break;
+        }
     }
+
     return path;
 }
