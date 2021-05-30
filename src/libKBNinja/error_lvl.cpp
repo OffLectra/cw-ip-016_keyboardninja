980c610146ae402df3b5f7e2a51a6d398257a949
Alexey <alexegoshin0403@yandex.ru>
2021-05-30 14:16:47 +0300

    Add comments and replased number to name button

--- src/libKBNinja/error_lvl.cpp
+++ src/libKBNinja/error_lvl.cpp
@@ -1,16 +1,25 @@
 #include "error_lvl.h"
+
+#include "constants.h"
 #include "getch.h"
+
 #include <iostream>
+
 using namespace std;
+
+/*
+error_lvl - this function displays a message that the level is closed
+*/
 
 void error_lvl()
 {
-    int input;
     cout << "Level close" << endl;
-
     cout << "For continue press Enter" << endl;
+
+    int input;
+
     do {
         input = getch();
-    } while (input != 10);
+    } while (input != enter);
     system("clear");
 }
