d0cea397b07ab33723245d6f209c91aa0fe658f3
Alexey <alexegoshin0403@yandex.ru>
2021-05-30 10:51:24 +0300

    Changed the names of variables and output in case of failure to open the file, in case of success-working with it

--- src/libKBNinja/load_save.cpp
+++ src/libKBNinja/load_save.cpp
@@ -9,20 +9,21 @@ using namespace std;
 void load_save(string& lang, int& lvl)
 {
     const string path = "src/resources/save.txt";
-    string save;
+    string str;
 
     ifstream savefile(path);
     if (!savefile.is_open())
         cout << "Error open file with save!" << endl;
-
-    int check = 0;
-    while (getline(savefile, save)) {
-        check++;
-        if (check == 1) {
-            lang = save;
-        }
-        if (check == 2) {
-            lvl = atoi(save.c_str());
+    else {
+        int current_str = 0;
+        while (getline(savefile, str)) {
+            current_str++;
+            if (current_str == 1) {
+                lang = str;
+            }
+            if (current_str == 2) {
+                lvl = atoi(str.c_str());
+            }
         }
     }
     savefile.close();
