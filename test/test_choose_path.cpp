b1c2c9ff94c4f600e73e1eaa23db436963fd1d00
Ilya <ilyazaytsev2002@bk.ru>
2021-06-14 18:06:09 +0700

    Added tests for English levels

--- test/test_choose_path.cpp
+++ test/test_choose_path.cpp
@@ -44,3 +44,44 @@ CTEST(choose_path, RU_11)
 
     ASSERT_STR(path.c_str(), result.c_str());
 }
+
+CTEST(choose_path, EN_2)
+{
+    string language = "EN";
+    int lvl = 2;
+    string path = "src/resources/symbol_EN.txt";
+    string result = choose_path(language, lvl);
+
+    ASSERT_STR(path.c_str(), result.c_str());
+}
+
+CTEST(choose_path, EN_6)
+{
+    string language = "EN";
+    int lvl = 6;
+    string path = "src/resources/words_EN.txt";
+    string result = choose_path(language, lvl);
+
+    ASSERT_STR(path.c_str(), result.c_str());
+}
+
+CTEST(choose_path, EN_7)
+{
+    string language = "EN";
+    int lvl = 7;
+    string path = "src/resources/sentence_EN.txt";
+    string result = choose_path(language, lvl);
+
+    ASSERT_STR(path.c_str(), result.c_str());
+}
+
+CTEST(choose_path, EN_12)
+{
+    string language = "EN";
+    int lvl = 12;
+    string path = "src/resources/paragraph_EN.txt";
+    string result = choose_path(language, lvl);
+
+    ASSERT_STR(path.c_str(), result.c_str());
+}
+
