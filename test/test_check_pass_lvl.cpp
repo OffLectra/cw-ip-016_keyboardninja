eb6695e0914738ab3e29ae1e9509cd1390b45705
Max <maxuswill@yandex.ru>
2021-06-15 06:42:34 -0400

    Add test Over_time_errors

--- test/test_check_pass_lvl.cpp
+++ test/test_check_pass_lvl.cpp
@@ -44,3 +44,17 @@ CTEST(pass_lvl, Over_errors)
 
     ASSERT_EQUAL(exp_result, result);
 }
+
+CTEST(pass_lvl, Over_time_errors)
+{
+    double current_time = 15.0;
+    double max_time = 11.0;
+    int current_errors = 5;
+    int max_errors = 3;
+
+    int exp_result = over_time_errors;
+    int result = check_pass_lvl(
+            current_time, max_time, current_errors, max_errors);
+
+    ASSERT_EQUAL(exp_result, result);
+}
