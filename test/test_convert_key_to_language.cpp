9c91fbde4426add4f18dd1d374328b43138a5ad9
Max <maxuswill@yandex.ru>
2021-06-15 06:58:41 -0400

    Add test RU

--- test/test_convert_key_to_language.cpp
+++ test/test_convert_key_to_language.cpp
@@ -13,3 +13,13 @@ CTEST(convert_key_to_language, EN)
 
     ASSERT_STR(exp_result.c_str(), result.c_str());
 }
+
+CTEST(convert_key_to_language, RU)
+{
+    int key = 0;
+    string exp_result = "RU";
+    string result = convert_key_to_language(key);
+
+    ASSERT_STR(exp_result.c_str(), result.c_str());
+}
+
