7342f82cf4d2fca4a54a018e110b2b24c67d5e46
Max <maxuswill@yandex.ru>
2021-06-15 06:34:15 -0400

    Add test win

---
+++ test/test_check_pass_lvl.cpp
@@ -0,0 +1,18 @@
+#include <ctest.h>
+
+#include <libKBNinja/check_pass_lvl.h>
+#include <libKBNinja/constants.h>
+
+CTEST(pass_lvl, Win)
+{
+    double current_time = 10.0;
+    double max_time = 11.0;
+    int current_errors = 0;
+    int max_errors = 3;
+
+    int exp_result = lvl_pass;
+    int result = check_pass_lvl(
+            current_time, max_time, current_errors, max_errors);
+
+    ASSERT_EQUAL(exp_result, result);
+}
